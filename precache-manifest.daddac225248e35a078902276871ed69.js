4b26cf65672031946c2d9c26181ee247a7e887f1
asc0910 <asc0910@hotmail.com>
2021-03-06 01:54:30 +0800

    add files
    add files

---
+++ precache-manifest.daddac225248e35a078902276871ed69.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f2f8fdb67f6a4a9302fd942d4970ca9b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5a0daaace5ab3b300465",
+    "url": "/static/js/2.6eef6421.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.6eef6421.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "706be6ed103f20e8436f",
+    "url": "/static/js/main.f6e9409c.chunk.js"
+  },
+  {
+    "revision": "b1dbf5e9804801549b870634fd7f4285",
+    "url": "/static/js/main.f6e9409c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "53a2798ec5b345984f88",
+    "url": "/static/js/runtime-main.2e2d5d07.js"
+  }
+]);
